95243e295e3085c9b22ca992022938ddaa75718f
Humayungithub <humayun2911@gmail.com>
2020-05-28 18:30:13 +0600

    Completed the 1st chapter matplotlib

---
+++ 02-intermediate-python-for-data-science/01-matplotlib/01-Chapter-Outline.py
@@ -0,0 +1,43 @@
+'''
+1. Matplotlib
+
+Data visualization is a key skill for aspiring data scientists. Matplotlib makes it easy to create meaningful and insightful plots. In this chapter, you’ll learn how to build various types of plots, and customize them to be more visually appealing and interpretable.
+
+Basic plots with Matplotlib
+
+Line plot (1)
+
+Line Plot (2): Interpretation
+
+Line plot (3)
+
+Scatter Plot (1)
+
+Scatter plot (2)
+
+Histogram
+
+Build a histogram (1)
+
+Build a histogram (2): bins
+
+Build a histogram (3): compare
+
+Choose the right plot (1)
+
+Choose the right plot (2)
+
+Customization
+
+Labels
+
+Ticks
+
+Sizes
+
+Colors
+
+Additional Customizations
+
+Interpretation
+'''
