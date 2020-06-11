d779592408b5bf99e3243041682852561dab27da
Humayungithub <humayun2911@gmail.com>
2020-06-11 22:05:06 +0600

    adding course description

---
+++ 02-intermediate-python-for-data-science/_course-description.py
@@ -0,0 +1,8 @@
+'''
+Course Description
+Learning Python is crucial for any aspiring data science practitioner. Learn to visualize real data with Matplotlib's functions and get acquainted with data structures such as the dictionary and the pandas DataFrame. After covering key concepts such as boolean logic, control flow, and loops in Python, you'll be ready to blend together everything you've learned to solve a case study using hacker statistics.
+
+4 hours
+18 Videos
+87 Exercises
+'''
