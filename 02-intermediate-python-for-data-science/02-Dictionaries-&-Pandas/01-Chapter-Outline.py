e742fdb60609ee9eefa3c11950affece2d9591f7
Humayungithub <humayun2911@gmail.com>
2020-06-11 21:44:06 +0600

    Completed the 2nd chapter Dictionaries & Pandas

---
+++ 02-intermediate-python-for-data-science/02-Dictionaries-&-Pandas/01-Chapter-Outline.py
@@ -0,0 +1,43 @@
+'''
+2. Dictionaries & Pandas
+
+Learn about the dictionary, an alternative to the Python list, and the pandas DataFrame, the de facto standard to work with tabular data in Python. You will get hands-on practice with creating and manipulating datasets, and you’ll learn how to access the information you need from these data structures.
+
+Dictionaries, Part 1
+
+Motivation for dictionaries
+
+Create dictionary
+
+Access dictionary
+
+Dictionaries, Part 2
+
+Dictionary Manipulation (1)
+
+Dictionary Manipulation (2)
+
+Dictionariception
+
+Pandas, Part 1
+
+Dictionary to DataFrame (1)
+
+Dictionary to DataFrame (2)
+
+CSV to DataFrame (1)
+
+CSV to DataFrame (2)
+
+Pandas, Part 2
+
+Square Brackets (1)
+
+Square Brackets (2)
+
+loc and iloc (1)
+
+loc and iloc (2)
+
+loc and iloc (3)
+'''
